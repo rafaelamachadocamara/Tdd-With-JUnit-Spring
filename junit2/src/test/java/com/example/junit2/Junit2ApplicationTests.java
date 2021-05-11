8d5ebb5648f94fc0484296a3ce76055a02a58f56
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-05-11 14:00:51 -0300

    Novos Testes Unitarios

---
+++ junit2/src/test/java/com/example/junit2/Junit2ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.junit2;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
